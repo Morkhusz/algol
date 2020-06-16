31a9d669d16a065ae58b10101c25d406a6d99a01
Morkhusz <--local>
2020-06-16 11:15:37 -0300

    Remove código desnecessário
    Foram removidos os métodos desnecessários para o desafio;
    Foram removidos os testes unitáiros referentes aos
    métodos removidos.

--- src/FizzBuzz/FizzBuzz.php
+++ src/FizzBuzz/FizzBuzz.php
@@ -4,39 +4,6 @@ namespace App\FizzBuzz;
 
 class FizzBuzz
 {
-    public function printFizzOnMultiplesOfThree(string $integers) : string
-    {
-        return implode(',', array_map(function ($int) {
-            if ($int % 3 === 0) {
-                return 'Fizz';
-            }
-
-            return $int;
-        }, explode(',', $integers)));
-    }
-
-    public function printBuzzOnMultiplesOfFive(string $integers) : string
-    {
-        return implode(',', array_map(function ($integer) {
-            if ($integer % 5 === 0) {
-                return 'Buzz';
-            }
-
-            return $integer;
-        }, explode(',', $integers)));
-    }
-
-    public function printFizzBuzzOnMultiplesOfThreeAndFive(string $integers) : string
-    {
-        return implode(',', array_map(function($integer) {
-            if ($integer % 15 === 0) {
-                return 'FizzBuzz';
-            }
-
-            return $integer;
-        }, explode(',', $integers)));
-    }
-
     public function fizzBuzz(string $integers) : string
     {
         $this->stringContainsOnlyCommaSeparatedIntegers($integers);
--- tests/FizzBuzz/FizzBuzzTest.php
+++ tests/FizzBuzz/FizzBuzzTest.php
@@ -11,45 +11,6 @@ class FizzBuzzTest extends TestCase
     /**
      * @test
      * For multiples of three output "Fizz" instead of the value
-     */
-    public function shouldPrintFizzOnMultiplesOfThree()
-    {
-        $fizzBuzz = new FizzBuzz();
-        $response = '1,2,Fizz,4,5,Fizz,7,8,Fizz,10';
-        $integers = '1,2,3,4,5,6,7,8,9,10';
-
-        $this->assertEquals($response, $fizzBuzz->printFizzOnMultiplesOfThree($integers));
-    }
-
-    /**
-     * @test
-     * For multiples of five output "Buzz".
-     */
-    public function shouldPrintBuzzOnMultiplesOfFive()
-    {
-        $fizzBuzz = new FizzBuzz();
-        $response = '1,2,3,4,Buzz,6,7,8,9,Buzz';
-        $integers = '1,2,3,4,5,6,7,8,9,10';
-
-        $this->assertEquals($response, $fizzBuzz->printBuzzOnMultiplesOfFive($integers));
-    }
-
-    /**
-     * @test
-     * For multiples of three and five output "FizzBuzz".
-     */
-    public function shouldPrintFizzBuzzOnMultiplesOfThreeAndFive()
-    {
-        $fizzBuzz = new FizzBuzz();
-        $response = '1,2,3,4,5,6,7,8,9,10,11,12,13,14,FizzBuzz';
-        $integers = '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15';
-
-        $this->assertEquals($response, $fizzBuzz->printFizzBuzzOnMultiplesOfThreeAndFive($integers));
-    }
-
-    /**
-     * @test
-     * For multiples of three output "Fizz" instead of the value
      * For multiples of five output "Buzz".
      * For multiples of three and five output "FizzBuzz".
      */
