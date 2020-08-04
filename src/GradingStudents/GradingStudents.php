839d7a351a57a4571c74b2259a3590a4124b5928
José Filho <jose-silvafilho@hotmail.com>
2020-08-04 12:30:59 -0300

    Implementa melhorias sugeridas na avaicacao do PR
    Remove metodo de validacao do input e traz para a proria funcao principal
    Remove utilizacao dos explode e implodes tratando a string de uma so vez

--- src/GradingStudents/GradingStudents.php
+++ src/GradingStudents/GradingStudents.php
@@ -6,29 +6,31 @@ use InvalidArgumentException;
 
 class GradingStudents
 {
-    public function roundGrades($grades)
+    public function roundGrades(string $grades) : string
     {
         $grades = explode(PHP_EOL, $grades);
         $students = (int) array_shift($grades);
-        $this->validateInput($students, $grades);
 
-        return implode(PHP_EOL, array_map(function ($grade) {
-            if ($grade < 0 || $grade > 100) {
-                throw new InvalidArgumentException('Grades should be a number between 0 and 100');
-            }
-            $rest = $grade % 5;
-            
-            return ($grade > 37 && $rest > 2) ? ($grade - $rest) + 5 : $grade;
-        }, $grades));
-    }
-
-    private function validateInput($students, $grades)
-    {
         if ($students < 1 || $students > 60) {
             throw new InvalidArgumentException('First line should be a positive integer between 1 and 60');
         }
         if (count($grades) !== $students) {
             throw new InvalidArgumentException('The grades amount should be equal to the studens amount');
         }
+        if (min($grades) < 0 || max($grades) > 100) {
+            throw new InvalidArgumentException('Grades should be a number between 0 and 100');
+        }
+
+        $roundedGrades = '';
+        $students--;
+        for ($i = 0; $i < $students; $i++) {
+            $rest = $grades[$i] % 5;
+            $roundedGrades .= ($grades[$i] > 37 && $rest > 2) ? ($grades[$i] - $rest) + 5 : $grades[$i];
+            $roundedGrades .= PHP_EOL;
+        }
+        $rest = $grades[$students] % 5;
+        $roundedGrades .= ($grades[$students] > 37 && $rest > 2) ? ($grades[$students] - $rest) + 5 : $grades[$students];
+
+        return $roundedGrades;
     }
 }
